3ed484200aee592b75131a24eff72ef7275148be
Ramzan Kurbanov <ramkurbanov10@yandex.ru>
2022-05-22 09:00:27 +0300

    add ingredient class and nested enum

---
+++ src/main/java/com/example/tacocloud/tacos/Ingredient.java
@@ -0,0 +1,17 @@
+package com.example.tacocloud.tacos;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+
+    public final String id;
+    public final String name;
+    public final Type type;
+
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
