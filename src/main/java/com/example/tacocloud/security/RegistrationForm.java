be46f442883262c1615392c1628e78b3df2a80a4
Ramzan Kurbanov <ramkurbanov10@yandex.ru>
2022-05-23 13:46:40 +0300

    add registration controller

---
+++ src/main/java/com/example/tacocloud/security/RegistrationForm.java
@@ -0,0 +1,23 @@
+package com.example.tacocloud.security;
+
+import com.example.tacocloud.User;
+import lombok.Data;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Data
+public class RegistrationForm {
+    public final String username;
+    public final String password;
+    public final String fullName;
+    public final String street;
+    public final String city;
+    public final String state;
+    public final String zip;
+    public final String phoneNumber;
+
+    public User toUser(PasswordEncoder passwordEncoder) {
+        return new User(
+                username, passwordEncoder.encode(password),
+                fullName, street, city, state, zip, phoneNumber);
+    }
+}
